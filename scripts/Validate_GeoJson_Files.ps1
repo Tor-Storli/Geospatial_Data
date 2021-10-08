7044725e0104263aefc2de8762eb8251bba074a1
Torbjorn Storli <storlitor@gmail.com>
2021-10-08 18:12:38 -0500

    New Files
    Added additional data and scripts.

---
+++ scripts/Validate_GeoJson_Files.ps1
@@ -0,0 +1,12 @@
+﻿
+# ==============================
+# Test Validity of GeoJson files
+# ============================== 
+$pathP = "C:\tmp\json"; 
+$files = Get-ChildItem  $pathP\*.json 
+$output = $files | ForEach-Object {
+                        $content = Get-Content $_.FullName
+                        $valid = $content | Test-Json
+                        "file: " + $_.FullName + "   valid: " + $valid
+                        }
+$output | Out-File "C:\tmp\json\powershell_scripts\Validate_GeoJson_files.txt"
